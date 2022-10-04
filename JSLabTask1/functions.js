76049e2409db903ca85c8e1146fe4a323ca95080
Arsal Shahzaib Bin Mushtaq <sp20-bcs-109@cuilahore.edu.pk>
2022-10-04 16:40:07 +0500

    ...

---
+++ JSLabTask1/functions.js
@@ -0,0 +1,31 @@
+
+// function btnTest() {
+//     alert("You CLicked a Button");
+//   }
+  
+//   function sortTodos() {
+//     let todoArray = [];
+  
+//     $("#todos li").each(function () {
+//       todoArray.push($(this).html());
+//     });
+  
+//     console.log(todoArray);
+//     todoArray.sort();
+//     $("#todos").empty();
+//     for (var i = 0; i < todoArray.length; i++) {
+//       $("#todos").append("<li>" + todoArray[i] + "</li>");
+//     }
+//     $("#todos").removeClass("green").addClass("red");
+// }
+
+
+function chkEmail() {
+    var getEmail = $("#email");
+
+    if (getEmail.val == "") {
+        $("#email").addClass("red");
+    } else {
+        
+    }
+}
