3f1b8822b3fc5e1b61b9d7cebf1755748458119f
Arsal Shahzaib Bin Mushtaq <sp20-bcs-109@cuilahore.edu.pk>
2022-10-28 01:53:03 +0500

    Create server.js

---
+++ Website/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+//if you dont have node_modules folder then run below command
+// npm install
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/contact-us", (req, res) => {
+    res.render("contact-us");
+});
+app.get("/", (req, res) => {
+    res.render("index");
+});
+app.listen(2000);
+
+//execute this file with following command
+// node server
+
+//for auto refresh of the code install nodemon globally
+//npm install nodemon -g
+// then run the file with nodemon
+//nodemon server
