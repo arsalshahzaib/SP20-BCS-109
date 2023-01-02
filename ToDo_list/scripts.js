6c3221a0aa4b40c111a6cc5027e4864d31340ca7
Arsal Shahzaib Bin Mushtaq <sp20-bcs-109@cuilahore.edu.pk>
2023-01-03 02:12:52 +0500

    update

--- ToDo_list/scripts.js
+++ ToDo_list/scripts.js
@@ -5,20 +5,35 @@ const todoList = document.getElementById('todo-list');
 let todos = [];
 
 addButton.addEventListener('click', () => {
-// Get the value of the input field
-    const todo = todoInput.value;
+  // Get the value of the input field
+  const todo = todoInput.value;
 
-// Add the new todo to the array
-    todos.push(todo);
+  // Add the new todo to the array
+  todos.push(todo);
 
-// Clear the input field
-    todoInput.value = '';
+  // Clear the input field
+  todoInput.value = '';
 
-// Update the HTML of the todo list
-    todoList.innerHTML = '';
-    todos.forEach(todo => {
-        const todoElem = document.createElement('li');
-        todoElem.textContent = todo;
-        todoList.appendChild(todoElem);
-    });
+  // Update the HTML of the todo list
+  updateTodoList();
 });
+
+function updateTodoList() {
+  todoList.innerHTML = '';
+  todos.forEach((todo, index) => {
+    const todoElem = document.createElement('li');
+    todoElem.textContent = todo;
+
+    // Add a delete button to each todo item
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      // Remove the todo from the array and update the HTML
+      todos.splice(index, 1);
+      updateTodoList();
+    });
+    todoElem.appendChild(deleteButton);
+
+    todoList.appendChild(todoElem);
+  });
+}
