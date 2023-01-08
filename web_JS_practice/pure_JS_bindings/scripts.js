205a7daad402b1ff64dbcbeb637b02d23da706a5
Arsal Shahzaib Bin Mushtaq <sp20-bcs-109@cuilahore.edu.pk>
2023-01-08 18:22:50 +0500

    Lecture 7 ended

--- web_JS_practice/pure_JS_bindings/scripts.js
+++ web_JS_practice/pure_JS_bindings/scripts.js
@@ -1,3 +1,19 @@
-function handleAddTask() {
-    console.log("Handling the Add Task button.");
-}
+// function handleAddTask() {
+//     console.log("Handling the Add Task button.");
+// }
+
+// function doBindings() {
+//     var button = document.getElementById("add-task");
+//     button.onclick = handleAddTask;
+// }
+
+window.onload = function() {
+    // do all the bindings here. :)
+    var button = document.getElementById("add-task");
+    button.onclick = function() {
+        console.log("Handling the Add Task button.");
+    };
+};
+
+
+// doBindings();
