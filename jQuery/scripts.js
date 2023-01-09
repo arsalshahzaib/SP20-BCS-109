743e00318b8451ee505a89165521f85bfc2b5fd1
Arsal Shahzaib Bin Mushtaq <sp20-bcs-109@cuilahore.edu.pk>
2023-01-09 19:08:08 +0500

    lecture 9 ends

--- jQuery/scripts.js
+++ jQuery/scripts.js
@@ -4,9 +4,22 @@ $(function() {
     // jQuery will execute this function after the page loading
     // so do your bindings here..
     $("#addButton").click(handleButtonClick);
+    $("#todo").on("click", "li", removeTODO);
 });
 
 function handleButtonClick() {
     // alert("Button Clicked.");
-    $("#todo").append
+    var newTODO = $("#add-new-todo").val();
+    if (!newTODO) {
+        $("#add-new-todo").addClass("error");
+        return;
+    }
+    $("#add-new-todo").removeClass("error");
+    $("#todo").append("<li>" + newTODO + "</li>");
+    $("#add-new-todo").val("");
+    // $("#todo li").click(removeTODO); DRY
+}
+
+function removeTODO() {
+    $(this).remove();
 }
