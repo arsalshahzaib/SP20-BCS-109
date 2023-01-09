ae466b069f6f58657e69432ab4ec1755acb6aeb2
Arsal Shahzaib Bin Mushtaq <sp20-bcs-109@cuilahore.edu.pk>
2023-01-09 23:17:36 +0500

    creating the API Client according to the Fake API i found on the internet

--- jQ_API_GET_Call/scripts.js
+++ jQ_API_GET_Call/scripts.js
@@ -7,7 +7,12 @@ function loadGames() {
         url: "https://jsonplaceholder.typicode.com/todos",
         method: "GET",
         success: function(response) {
-            console.log(response);
+            // console.log(response);
+            var todosL = $("#todo");
+            todosL.empty();
+            for (let index = 0; index < response.length; index++) {
+                todosL.append("<div><h3>-> " + response[index].title + "</h3></div>");
+            }
         }
     });
 }
