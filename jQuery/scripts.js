4cc25ae7e0c856e53ea707404c7c2303ca6d2e28
Arsal Shahzaib Bin Mushtaq <sp20-bcs-109@cuilahore.edu.pk>
2023-01-09 18:23:30 +0500

    lec 9

---
+++ jQuery/scripts.js
@@ -0,0 +1,12 @@
+// jQuery practice
+
+$(function() {
+    // jQuery will execute this function after the page loading
+    // so do your bindings here..
+    $("#addButton").click(handleButtonClick);
+});
+
+function handleButtonClick() {
+    // alert("Button Clicked.");
+    $("#todo").append
+}
